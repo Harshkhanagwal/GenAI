149db5163385a7529057471263bfc08bc454dbe3
harsh khanagwal <harshkhanagwall29@gmail.com>
2026-08-13 21:57:44 +0530

    embedding with sentance transformer

--- rag-from-scratch/main.py
+++ rag-from-scratch/main.py
@@ -1,4 +1,6 @@
+from sentence_transformers import SentenceTransformer
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sklearn.metrics.pairwise import cosine_similarity
 
 with open("data/sample.txt", "r") as file:
     text = file.read()
@@ -11,7 +13,29 @@ splitter = RecursiveCharacterTextSplitter(
 
 chunks = splitter.split_text(text)
 
-for i, chunk in enumerate(chunks):
-    print(f"Chunk {i + 1} | Length: {len(chunk)}")
-    print(chunk)
-    print("-" * 50)
+# for i, chunk in enumerate(chunks):
+#     print(f"Chunk {i + 1} | Length: {len(chunk)}")
+#     print(chunk)
+#     print("-" * 50)
+
+
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# first_chunk = chunks[0]
+# embedding = model.encode(first_chunk)
+
+# print(type(embedding))
+# print(len(embedding))
+# print(embedding[:10])
+
+# embeddedChunks = []
+# for chunk in chunks:
+#     embeddedChunks.append(model.encode(chunk))
+
+# # print(embeddedChunks)
+# print(embeddedChunks.shape)
+
+question = "What is Retrieval Augmented Generation?"
+
+quesEmbedding = model.encode(question)
